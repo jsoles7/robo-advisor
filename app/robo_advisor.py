2418a5f0e49696c8772f7c940c91fa936ca6103d
jsoles7 <45300260+jsoles7@users.noreply.github.com>
2020-02-17 22:24:09 -0500

    added some utility with output times

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,7 +3,10 @@
 #import certain modules/ packages
 import requests
 import json 
+from datetime import datetime
 
+#defining key variables
+now = datetime.now()
 
 #scrapping what is required from web 
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo "
@@ -13,10 +16,11 @@ response = requests.get(request_url)
 print(response.status_code) #> 200
 #print(response.text)
 
-#parse 
+#parse from the response text into dictionary
 parsed_response = json.loads(response.text)
+#print(parsed_response)
+latest_refresh = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-print(parsed_response)
 #receiving client inputs
  
 
@@ -26,9 +30,9 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: " + now.strftime("%Y-%m-%d %I:%M %p"))
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print("LATEST DAY: " + latest_refresh )
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
