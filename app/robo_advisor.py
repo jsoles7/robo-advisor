248439c61494864efbeb78f5b5e62301aa69b295
jsoles7 <45300260+jsoles7@users.noreply.github.com>
2020-02-18 20:38:07 -0500

    integrated my program with some code from Alpha Vantage in order to display stock charts

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -35,6 +35,11 @@ ALPHA_VANTAGE_API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY")
 
 
 #receiving client inputs
+print("")
+print("Welcome to the FinServ 2.0 Robo Stock Advisor, where quality advice is offered for free!")
+print("")
+print("To use this program, input a stock ticker of your choice and receive a recommendation based on our proprietary algorithms")
+print("")
 symbol = input(print("Please input a stock ticker for a stock you which to get advice on: "))
 
 
@@ -79,10 +84,9 @@ column_names= ["Timestamp", "Open", "High", "Low", "Close", "Volume"]
 
 
 #write in the items to the file
-
-
 with open(csv_filepath, "w") as file:
     writer = csv.DictWriter(file, column_names)
+    #write in the headers
     writer.writeheader()
     for date in time_series_keys:
         #loop each different timestamp row using this established row format 
@@ -98,8 +102,9 @@ with open(csv_filepath, "w") as file:
 
 file.close()
 
-#recent high calculations
 
+
+#recent high/ low calculations
 #define local variables
 max = 0.0
 min = 1000.0
@@ -137,4 +142,26 @@ print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+print("")
+print("")
 
+
+#data visualization
+#this was adopted from the alpha vantage customer service website/ github
+
+key = ALPHA_VANTAGE_API_KEY
+ts = TimeSeries(key, output_format='pandas')
+ti = TechIndicators(key)
+
+# Get the data, returns a tuple
+# aapl_data is a pandas dataframe, aapl_meta_data is a dict
+aapl_data, aapl_meta_data = ts.get_daily(symbol=symbol)
+# aapl_sma is a dict, aapl_meta_sma also a dict
+aapl_sma, aapl_meta_sma = ti.get_sma(symbol=symbol)
+
+# Visualization
+figure(num=None, figsize=(15, 6), dpi=80, facecolor='w', edgecolor='k')
+aapl_data['4. close'].plot()
+plt.tight_layout()
+plt.grid()
+plt.show()
