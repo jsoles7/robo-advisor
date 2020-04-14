e262f33d09a1d5c7a95ee938c56c7ce7fcf07a23
jsoles7 <45300260+jsoles7@users.noreply.github.com>
2020-04-13 21:56:38 -0400

    added 3 more tests + found a better way to design min/ max calculations

--- test/test_robo_advisor.py
+++ test/test_robo_advisor.py
@@ -1,6 +1,6 @@
 
 
-from app.robo_advisor import to_usd, algo_output, input_validation, request_url, adj_low_volume, adj_low_price, min_calc
+from app.robo_advisor import to_usd, algo_output, input_validation, request_url, adj_low_volume, adj_low_price, movement_calc, min_calc, max_calc
 
 def test_to_usd():
     #Should apply correct formatting
@@ -49,3 +49,141 @@ def test_adj_low_volume():
     vol = adj_low_volume(100)
     assert vol == 20
 
+def test_movement_calc():
+    #Should return the % change
+    latest = 25 
+    average = 20
+    assert movement_calc(latest, average) == .25
+
+def test_min_calc():
+    response = {
+    "Meta Data": {
+        "1. Information": "Daily Time Series with Splits and Dividend Events",
+        "2. Symbol": "IBM",
+        "3. Last Refreshed": "2020-04-13",
+        "4. Output Size": "Compact",
+        "5. Time Zone": "US/Eastern"
+    },
+    "Time Series (Daily)": {
+        "2020-04-13": {
+            "1. open": "121.6300",
+            "2. high": "121.8000",
+            "3. low": "118.0400",
+            "4. close": "121.1500",
+            "5. adjusted close": "121.1500",
+            "6. volume": "5087275",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0000"
+        },
+        "2020-04-09": {
+            "1. open": "120.4800",
+            "2. high": "122.9200",
+            "3. low": "120.1672",
+            "4. close": "121.5000",
+            "5. adjusted close": "121.5000",
+            "6. volume": "5576210",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0000"
+        },
+        "2020-04-08": {
+            "1. open": "116.3100",
+            "2. high": "119.9600",
+            "3. low": "115.0742",
+            "4. close": "119.2900",
+            "5. adjusted close": "119.2900",
+            "6. volume": "5155987",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0000"
+        },
+        "2020-04-07": {
+            "1. open": "118.8000",
+            "2. high": "119.5700",
+            "3. low": "114.8700",
+            "4. close": "114.9400",
+            "5. adjusted close": "114.9400",
+            "6. volume": "5592463",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0000"
+        },
+        "2020-04-06": {
+            "1. open": "110.3500",
+            "2. high": "115.6300",
+            "3. low": "110.1300",
+            "4. close": "114.8200",
+            "5. adjusted close": "114.8200",
+            "6. volume": "7026457",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0000"
+        }
+        }
+    }
+    keys_list = ["2020-04-13","2020-04-09", "2020-04-08","2020-04-07","2020-04-06"]
+    #Should return the minimum low
+    assert min_calc(5, response, keys_list) == 110.1300
+
+
+def test_max_calc():
+    response = {
+    "Meta Data": {
+        "1. Information": "Daily Time Series with Splits and Dividend Events",
+        "2. Symbol": "IBM",
+        "3. Last Refreshed": "2020-04-13",
+        "4. Output Size": "Compact",
+        "5. Time Zone": "US/Eastern"
+    },
+    "Time Series (Daily)": {
+        "2020-04-13": {
+            "1. open": "121.6300",
+            "2. high": "121.8000",
+            "3. low": "118.0400",
+            "4. close": "121.1500",
+            "5. adjusted close": "121.1500",
+            "6. volume": "5087275",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0000"
+        },
+        "2020-04-09": {
+            "1. open": "120.4800",
+            "2. high": "122.9200",
+            "3. low": "120.1672",
+            "4. close": "121.5000",
+            "5. adjusted close": "121.5000",
+            "6. volume": "5576210",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0000"
+        },
+        "2020-04-08": {
+            "1. open": "116.3100",
+            "2. high": "119.9600",
+            "3. low": "115.0742",
+            "4. close": "119.2900",
+            "5. adjusted close": "119.2900",
+            "6. volume": "5155987",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0000"
+        },
+        "2020-04-07": {
+            "1. open": "118.8000",
+            "2. high": "119.5700",
+            "3. low": "114.8700",
+            "4. close": "114.9400",
+            "5. adjusted close": "114.9400",
+            "6. volume": "5592463",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0000"
+        },
+        "2020-04-06": {
+            "1. open": "110.3500",
+            "2. high": "115.6300",
+            "3. low": "110.1300",
+            "4. close": "114.8200",
+            "5. adjusted close": "114.8200",
+            "6. volume": "7026457",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0000"
+        }
+        }
+    }
+    keys_list = ["2020-04-13","2020-04-09", "2020-04-08","2020-04-07","2020-04-06"]
+    #Should return the maximum high
+    assert max_calc(5, response, keys_list) == 122.9200
